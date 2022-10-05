d0eac8674a33a947d19bdd5299a47a5e9d42bc3a
Angelargv <argaitan@uninorte.edu.co>
2022-10-05 17:03:07 -0500

    Se agregan usuarios a la base y se loguean por usuario

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 import sqlite3
+import hashlib
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
@@ -11,6 +14,41 @@ def principal():
 def login():
     return render_template("login.html")
 
+@app.route("/loguearse",methods=["post"])
+def log():
+    error = []
+    # Captura los datos enviados
+    username = request.form["txtusername"]
+    password = request.form["txtpassword"]
+
+    # Validaciones
+    if not username or not password:
+        error.append("Usuario/Contraseña son requeridos")
+        return render_template("login.html", error = error)  
+    else:
+
+        clave = hashlib.sha256(password.encode())
+        pwd = clave.hexdigest()
+
+        #Conexión a BD
+        with sqlite3.connect("redsocial.db") as con:
+            # Convierte el registro en un diccionario
+            con.row_factory = sqlite3.Row
+            cur = con.cursor()
+           
+            # Sentencias preparadas
+            cur.execute("SELECT * FROM usuarios WHERE usuario= ? AND contraseña= ?",[username, pwd])
+            # cur.fetchone()
+            if cur.fetchone():
+                return redirect("/login/perfil")
+            else:
+                error.append("Usuario o contraseña no existe, por favor registrate")
+                return render_template("login.html", error = error) 
+
+        
+      
+
+
 @app.route("/registrarse")
 def registrarse():
     return render_template("registro.html")
@@ -21,13 +59,48 @@ def registro():
     edad = request.form["txtedad"]
     usuario = request.form["txtusuario"]
     contrasena = request.form["txtcontrasena"]
-    selec_pregunta = request.form["select"]
-    res_pregunta = request.form["txtrespuestapregunta"]
+    comprobar = request.form["txtcomprobacion"]
+
+    if not correo:
+        return "Debe digitar un correo"
+
+    if not edad:
+        return "Debe digitar su edad"
+
+    if not usuario:
+        return "Debe digitar un usuario"
+
+    if not contrasena:
+        return "Debe digitar un contraseña"
+
+    if (contrasena!= comprobar):
+        return "Contraseña no coincide"
+
+    # Aplica la función hash (haslib) al password
+    clave = hashlib.sha256(contrasena.encode())
+    # Convierte el password a hexadecimal tipo string
+    pwd = clave.hexdigest()
+    # Se conecta a la BD
     with sqlite3.connect("redsocial.db") as con:
         cur = con.cursor()
-        cur.execute("INSERT INTO usuarios (correo,edad,usuario,contraseña,preguntaseguridad,respuestapreseguridad) VALUES (?,?,?,?,?,?)",[correo,edad,usuario,contrasena,selec_pregunta, res_pregunta])
+        # Consultar si ya existe Usuario
+        if siExiste(usuario):
+            return "Ya existe el Usuario!"
+        #Crea el nuevo Usuario
+        cur.execute("INSERT INTO usuarios (correo,edad,usuario,contraseña) VALUES (?,?,?,?)",[correo,edad,usuario,pwd])
         con.commit()
-    return "Guardado"
+        return redirect ("/login/perfil")
+
+def siExiste(user):
+     # Se conecta a la BD
+    with sqlite3.connect("redsocial.db") as con:
+        cur = con.cursor()
+        # Consultar si ya existe Usuario
+        cur.execute("SELECT usuario FROM usuarios WHERE usuario=?",[user])
+        if cur.fetchone():
+            return True
+    
+    return False
 
 @app.route("/login/perfil")
 def perfil():
